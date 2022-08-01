ce6a9243cc1fb7081aff0f24f7e4a827ea57ea54
Forjekes <1263232@mail.ru>
2022-08-02 00:54:34 +0400

    Сделал функцию is_admin(айди пользователя, event), которая возвращает 1, если пользователь админ и 0 если нет.

--- app/bot.py
+++ app/bot.py
@@ -8,15 +8,32 @@ from vk_events import send_message
 from vk_config import token_vk
 
 
-session = vk_api.VkApi(token=token_vk)
+session = vk_api.VkApi(token=vk_token)
 
 
-def start():
+ def is_admin(id_p, event_p):
 
+     flag = 0
+     Group_id = session.method("groups.getById", {"peer_id": event_p.peer_id})
+     GroupInf = session.method("groups.getMembers", {"group_id": Group_id[0]["id"], "filter": "managers"})
+
+     for Member in GroupInf["items"]:
+         if Member["id"] == id_p and (Member["role"] == "administrator" or "creator"):
+             flag = 1
+
+     return flag
+
+
+ def start():
     for event in VkLongPoll(session).listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             user_id = event.user_id
-            text = event.text.lower()
+             text = event.text.lower()
 
-            if text == "start":
-                send_message(session, user_id, "Hello!")
+             if text == "start":
+                 if is_admin(user_id, event) == 1:
+                     send_message(session, user_id, "Hi, admin!")
+                 else:
+                     send_message(session, user_id, "Hi, user!")
+
+ start()
