d8955dbf28c635b184d557fb8307ce4243832520
Forjekes <1263232@mail.ru>
2022-08-03 13:52:21 +0400

    fix: added registration

--- app/bot.py
+++ app/bot.py
@@ -11,12 +11,31 @@ from vk_config import token_vk
 session = vk_api.VkApi(token=token_vk)
 
 
+def is_admin(id_p, event_p):
+
+    group_id = session.method("groups.getById", {"peer_id": event_p.peer_id})
+    group_inf = session.method("groups.getMembers", {"group_id": group_id[0]["id"], "filter": "managers"})
+
+    for member in group_inf["items"]:
+
+        if member["id"] == id_p and (member["role"] == "administrator" or "creator"):
+            return 1
+
+    return 0
+
+
 def start():
 
     for event in VkLongPoll(session).listen():
+
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+
             user_id = event.user_id
             text = event.text.lower()
 
             if text == "start":
-                send_message(session, user_id, "Hello!")
+
+                if is_admin(user_id, event) == 1:
+                    send_message(session, user_id, "Hi, admin!")
+                else:
+                    send_message(session, user_id, "Hi, user!")
