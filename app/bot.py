9803ac1ea5627bf8298e6ae543b14b0610ee1c49
Danila Smirnov <ylptred@gmail.com>
2022-08-09 22:43:04 +0300

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -4,29 +4,20 @@ import psycopg2
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
+
 from vk_tools import Keyboard
 from vk_events import send_message
 from vk_config import token_vk
-from vk_config import db_name, user_name, pswrd, host, port
+from create_db import engine, get_session, guests, orgs, groups, info, tech_support, sendings
 
 
-session = vk_api.VkApi(token=token_vk)
-
-connection = psycopg2.connect(
-        database=db_name,
-        user=user_name,
-        password=pswrd,
-        host=host,
-        port=port
-    )
-
-cursor = connection.cursor()
+vk_session = vk_api.VkApi(token=token_vk)
 
 
 def is_admin(id_p, event_p):
 
-    group_id = session.method("groups.getById", {"peer_id": event_p.peer_id})
-    group_inf = session.method("groups.getMembers", {"group_id": group_id[0]["id"], "filter": "managers"})
+    group_id = vk_session.method("groups.getById", {"peer_id": event_p.peer_id})
+    group_inf = vk_session.method("groups.getMembers", {"group_id": group_id[0]["id"], "filter": "managers"})
 
     for member in group_inf["items"]:
 
@@ -40,7 +31,7 @@ def is_admin(id_p, event_p):
 
 def start():
 
-    for event in VkLongPoll(session).listen():
+    for event in VkLongPoll(vk_session).listen():
 
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
 
@@ -50,7 +41,8 @@ def start():
 
             if text == "start":
 
+
                 if is_admin(user_id, event):
-                    send_message(session, user_id, "Hi, admin!")
+                    send_message(vk_session, user_id, "Hi, admin!")
                 else:
-                    send_message(session, user_id, "Hi, user!")
+                    send_message(vk_session, user_id, "Hi, user!")
